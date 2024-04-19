3d87a9a2abbb31790073aadd562f93ebef6120d9
MuhammedHussain33 <128350536+MuhammedHussain33@users.noreply.github.com>
2024-04-19 16:39:14 +0530

    last commit

--- backend/utils/genarateToken.js
+++ backend/utils/genarateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId,res)=>{
             maxAge:15 * 24 * 60 * 60 *1000,//ms
             httpOnly:true ,    /// prevent XSS attacks cross-site scripting attacks
             sameSite:"strict" ,  /// csrf attacks cross-site forgery attack
-            secure:process.env.NODE_ENV !== "development"?true : false,
+            secure:process.env.NODE_ENV !== "development",
         })
 }
 
